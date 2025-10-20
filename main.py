ee55e7038b5ede64ef20e0864b948f10cfdf435c
Kevin <kevin@comtor.net>
2025-10-20 00:18:53 -0500

    cambio menor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from src.config.base import Base
 
-from src.routes import auditoria_route, comprobante_route, usuario_route, movimiento_route, item_route, ubicacion_route
+from src.routes import auditoria_route, comprobante_route, usuario_route, movimiento_route, item_route, ubicacion_route, existencia_route
 app = FastAPI(title="Farma Gestión Backend")
 
 app.include_router(auditoria_route.router)
@@ -10,4 +10,5 @@ app.include_router(usuario_route.router)
 app.include_router(movimiento_route.router)
 app.include_router(item_route.router)
 app.include_router(ubicacion_route.router)
+app.include_router(existencia_route.router)
 
